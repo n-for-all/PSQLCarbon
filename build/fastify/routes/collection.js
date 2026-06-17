1c4f9aba2defda951b9a7d08e270ff84153567aa
Naji Amer <icu090@gmail.com>
2026-06-18 02:34:01 +0400

    changes

--- build/fastify/routes/collection.js
+++
@@ -1,8 +0,0 @@
-export default async (app) => {
-    app.get("/collections", async (request, reply) => {
-        return reply.send({ collections: ["collection1", "collection2"] });
-    });
-    app.get("/test", async (request, reply) => {
-        return reply.send({ collections: ["collection1", "collection2"] });
-    });
-};
